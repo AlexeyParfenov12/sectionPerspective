80253c5d1711255a968092f15444091105234b8c
Parfenov Alexey <98681378+AlexeyParfenov12@users.noreply.github.com>
2026-03-04 04:33:16 +0000

    resultat

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ let test = document.querySelector('.test')
 
 section.forEach(element => {
     document.addEventListener('mousemove', (e) => {
-        if(element.getBoundingClientRect().x > 150){
-            let clientX = e.clientX * 0.04;
-            let clientY = e.clientY * 0.04;
-            let prClientX = e.clientX * 0.000001;
-            let prClientY = e.clientY * 0.000001;
-            // console.log(prClientY)
-            console.log(element.getBoundingClientRect().x)
+        if(element.getBoundingClientRect().x > window.innerWidth/2){
+            let clientX = e.clientX * 0.03;
+            let clientY = e.clientY * 0.03;
+            let prClientX = e.clientX * 0.0000013;
+            let prClientY = e.clientY * 0.0000013;
             let matrixValue = [
                                 0, 1, 0, prClientY, 
                                 1, 0, 0, prClientX, 
@@ -19,13 +17,11 @@ section.forEach(element => {
                                 clientX, clientY, 0, 1
             ];
             element.style.transform = `matrix3d(${matrixValue.join(', ')})`;
-        }else if(element.getBoundingClientRect().x < 50){
-            let clientX = e.clientX * 0.04 - 0.01;
-            let clientY = e.clientY * 0.04 - 0.01;
-            let prClientX = e.clientX * 0.000001 - 0.0007;
-            let prClientY = e.clientY * 0.000001;
-            // console.log(prClientY)
-            // console.log(element.getBoundingClientRect().x)
+        }else if(element.getBoundingClientRect().x < window.innerWidth/2){
+            let clientX = e.clientX * 0.03 - 0.01;
+            let clientY = e.clientY * 0.03 - 0.01;
+            let prClientX = e.clientX * 0.0000013 - 0.0007;
+            let prClientY = e.clientY * 0.0000013;
             let matrixValue = [
                 0, 1, 0, prClientY, 
                 1, 0, 0, prClientX, 
@@ -38,8 +34,6 @@ section.forEach(element => {
             let clientY = e.clientY * 0.01;
             let prClientX = e.clientX * 0.0000001;
             let prClientY = e.clientY * 0.0000001;
-            // console.log(prClientY)
-            // console.log(element.getBoundingClientRect().x)
             let matrixValue = [
                 0, 1, 0, prClientY, 
                 1, 0, 0, prClientX, 
